56d4c2d981154070f2b19faedf4d8c91732cbe55
LaxmiVatsalyaDaita <87368740+LaxmiVatsalyaDaita@users.noreply.github.com>
2021-07-13 17:07:03 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,42 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const cors = require("cors");
+
+const io = require("socket.io")(server, {
+	cors: {
+		origin: "*",
+		methods: [ "GET", "POST" ]
+	}
+});
+
+app.use(cors());
+app.set('view engine', 'ejs')
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+	console.log(req.oidc.isAuthenticated());
+	res.render("bleh", {
+		isAuthenticated: req.oidc.isAuthenticated(),
+	
+	});
+	
+});
+
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id);
+
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	});
+
+	socket.on("callUser", ({ userToCall, signalData, from, name }) => {
+		io.to(userToCall).emit("callUser", { signal: signalData, from, name });    
+	});
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	});
+});
+
+server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
